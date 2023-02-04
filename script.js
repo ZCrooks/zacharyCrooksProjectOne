580773d371316210af3b754cbd7cc94059714086
ZCrooks <zachthedevguy@gmail.com>
2023-02-04 12:58:43 -0500

    added script for automatic background color change on homepage header

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ popupForm.addEventListener('submit', function() {
     alert ("Thank you for signing up to our newsletter! Vibes coming your way.")
 })
 
+// When Home page is reloaded - Generate pre-set background colours in header which will change automatically 
+let i = 0;
+let colors = ['#b9dde7', '#f38d8d', '#68b0c1', '#227284'] ;
+
+function changeColor () {
+    const background = document.getElementById('home');
+    background.style.backgroundColor = colors[i];
+    i = i + 1;
+    if (i > 3) {
+        i = 0;
+    }
+    interval ();
+}
+
+function interval () {
+    return setTimeout(changeColor, 2000);
+}
+
+interval();
